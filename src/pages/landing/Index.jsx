a3da6575b2339e0e1f2942ffd8b48a7a238782d3
felipecalori <felipecalori92@gmail.com>
2022-07-05 20:08:45 -0300

    feat: component button e input

--- src/pages/landing/Index.jsx
+++ src/pages/landing/Index.jsx
@@ -1,3 +1,53 @@
-export default function Landing () {
-    return <><p>Landing</p></>
+import Button from "../../components/button";
+import Input from "../../components/input";
+import {
+  Container,
+  FormBox,
+  Fundo,
+  Header,
+  ImgBox,
+  StyledForm,
+} from "./styles";
+
+export default function Landing() {
+  return (
+    <Fundo>
+      <Header>
+        <img src="" alt="logo" />
+        <nav>
+          <ul>
+            <li>Sobre o projeto</li>
+            <li>Deduzindo impostos</li>
+            <li>Campanha populares</li>
+            <li>Sobre a equipe</li>
+          </ul>
+        </nav>
+      </Header>
+      <Container>
+        <ImgBox />
+        <FormBox>
+          <StyledForm>
+            <h2>Entre ou crie sua conta</h2>
+
+            <Input>
+              <label>Nome</label>
+              <input type="text"></input>
+            </Input>
+            <Input>
+              <label>Senha</label>
+              <input tupe="password"></input>
+            </Input>
+            <Button>Entrar</Button>
+            <span>
+              Não tem uma conta? <a>Cadastrar-se</a>
+            </span>
+          </StyledForm>
+        </FormBox>
+      </Container>
+      <div>
+        <p>DESÇA PARA SABER MAIS</p>
+        <img src="" alt="set para baixo" />
+      </div>
+    </Fundo>
+  );
 }
--- src/style/Theme.jsx
+++ src/style/Theme.jsx
@@ -1,22 +1,22 @@
 const lightTheme = {
-    color: "#fffff",
-    backgroundColor: "#000000",
-    button: {
-      textColor: "#D3D4E6",
-      borderColor: "#151A69",
-      backgroundColor: "#151A69",
-    },
-  };
-  const darkTheme = {
-    color: "#D3D4E6",
+  color: "#000000",
+  blueGradient: "linear-gradient(114.83deg, #246097 0%, #0A2260 99.64%)",
+  button: {
+    textColor: "#D3D4E6",
+    borderColor: "#151A69",
+    backgroundColor: "#151A69",
+  },
+};
+const darkTheme = {
+  color: "#D3D4E6",
+  blueGradient: "linear-gradient(114.83deg, #246097 0%, #0A2260 99.64%)",
+  button: {
+    textColor: "#151A69",
+    borderColor: "#151A69",
     backgroundColor: "#39424E",
-    button: {
-      textColor: "#151A69",
-      borderColor: "#151A69",
-      backgroundColor: "#39424E",
-    },
-  };
-  export const themes = {
-    light: lightTheme,
-    dark: darkTheme,
-  };
+  },
+};
+export const themes = {
+  light: lightTheme,
+  dark: darkTheme,
+};
