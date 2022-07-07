6765e1553d8edbf171e43abb0416ca7037d89e7c
Fábio Casanova <98760788+Fabiojcp@users.noreply.github.com>
2022-07-07 19:41:59 -0300

    feat: register page

--- src/pages/registration/Index.jsx
+++ src/pages/registration/Index.jsx
@@ -10,9 +10,14 @@ import {
   P,
   Form,
   FormTitle,
+  DivPassword,
+  Select,
+  Option,
+  Bio,
 } from "./style";
 import { Logo } from "../landing/styles";
 import { Link } from "react-router-dom";
+import Input from "../../components/input";
 
 export default function Registration() {
   const [type, setType] = useState("");
@@ -39,15 +44,161 @@ export default function Registration() {
               <Button onClick={() => setType("juridica")}>
                 Sou uma pessoa jurídica
               </Button>
-              <Button onClick={() => setType("entity")}>
+              <Button onClick={() => setType("entidade")}>
                 Sou uma organização
               </Button>
             </>
           )}
           {type === "fisica" && (
-          <>
-          
-          </>)}
+            <>
+              <Input>
+                <label>Nome completo</label>
+                <input type="text"></input>
+              </Input>
+              <Input>
+                <label>E-mail</label>
+                <input type="text"></input>
+              </Input>
+              <DivPassword>
+                <Input>
+                  <label>Senha</label>
+                  <input type="text"></input>
+                </Input>
+                <Input>
+                  <label>Repita senha</label>
+                  <input type="text"></input>
+                </Input>
+              </DivPassword>
+              <Input>
+                <label>Avatar (url)</label>
+                <input type="text"></input>
+              </Input>  
+              <Input>
+                <label>Telefone para contato</label>
+                <input type="text"></input>
+              </Input>
+              <Select>
+                <Option>Homem cis</Option>
+                <Option>Homem trans</Option>
+                <Option>Mulher cis</Option>
+                <Option>Mulher cis</Option>
+                <Option>Prefiro não declarar / Outro</Option>
+              </Select>
+              <Bio >
+                <label>Bio</label>
+                <textarea type="text" rows="8"></textarea>
+              </Bio>
+              <Button>Criar conta</Button>
+            </>
+          )}
+
+          {type === "juridica" && (
+            <>
+              <Input>
+                <label>Nome fantasia</label>
+                <input type="text"></input>
+              </Input>
+              <Input>
+                <label>E-mail</label>
+                <input type="text"></input>
+              </Input>
+              <DivPassword>
+                <Input>
+                  <label>Senha</label>
+                  <input type="text"></input>
+                </Input>
+                <Input>
+                  <label>Repita senha</label>
+                  <input type="text"></input>
+                </Input>
+              </DivPassword>
+              <Input>
+                <label>Avatar (url)</label>
+                <input type="text"></input>
+              </Input>
+              <Input>
+                <label>Pessoa de contato</label>
+                <input type="text"></input>
+              </Input>
+              <Input>
+                <label>Telefone para contato</label>
+                <input type="text"></input>
+              </Input>
+              <Bio >
+                <label>Bio</label>
+                <textarea type="text" rows="8"></textarea>
+              </Bio>
+              <Button>Criar conta</Button>
+            </>
+          )}
+
+          {type === "entidade" && (
+            <>
+              <Input>
+                <label>Nome fantasia</label>
+                <input type="text"></input>
+              </Input>
+              <Input>
+                <label>E-mail</label>
+                <input type="text"></input>
+              </Input>
+              <DivPassword>
+                <Input>
+                  <label>Senha</label>
+                  <input type="text"></input>
+                </Input>
+                <Input>
+                  <label>Repita senha</label>
+                  <input type="text"></input>
+                </Input>
+              </DivPassword>
+              <Input>
+                <label>Avatar (url)</label>
+                <input type="text"></input>
+              </Input>
+              <Input>
+                <label>Pessoa de contato</label>
+                <input type="text"></input>
+              </Input>
+              <Input>
+                <label>Telefone para contato</label>
+                <input type="text"></input>
+              </Input>
+              <FormTitle>Dados bancários</FormTitle>
+              <DivPassword>
+                <Input>
+                  <label>Agência</label>
+                  <input type="text"></input>
+                </Input>
+                <Input>
+                  <label>Conta</label>
+                  <input type="text"></input>
+                </Input>
+              </DivPassword>
+
+              <Input>
+                <label>Banco</label>
+                <input type="text"></input>
+              </Input>
+              <Input>
+                <label for="tipo">Tipo</label>
+                <Select name="tipo">
+                  <Option>Corrente</Option>
+                  <Option>Poupança</Option>
+                </Select>
+              </Input>
+
+              <Input>
+                <label>Chave pix</label>
+                <input type="text"></input>
+              </Input>
+              <Bio >
+                <label>Bio</label>
+                <textarea type="text" rows="8"></textarea>
+              </Bio>
+              <Button>Criar conta</Button>
+            </>
+          )}
         </Form>
       </DivForm>
     </DivMain>
--- src/pages/registration/style.js
+++ src/pages/registration/style.js
@@ -3,8 +3,9 @@ import styled from "styled-components";
 
 export const DivMain = styled.div`
   background: linear-gradient(114.83deg, #246097 0%, #0a2260 99.64%);
-  height: 100vh;
+  min-height: 100vh;
   width: 100vw;
+  padding: 0 0 8vh 0;
 `;
 
 export const DivHeader = styled.div`
@@ -65,4 +66,55 @@ export const Form = styled.form`
 export const FormTitle = styled.p`
   text-align: center;
   margin: 0 0 1vh 0;
+`;
+
+export const DivPassword = styled.div`
+  display: flex;
+  flex-direction: row;
+  div:nth-child(1) {
+    margin: 0 1vw 0 0;
+  }
+`;
+
+export const Select = styled.select`
+  font-weight: bold;
+  color: white;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.3);
+  padding: 15px 10%;
+  border-radius: 8px;
+  border: 2px solid white;
+  cursor: pointer;
+  font-weight: 400;
+  font-size: 1.5rem;
+`;
+
+export const Option = styled.option`
+  color: black;
+  background-color: rgba(255, 255, 255, 0.1);
+  backdrop-filter: blur(0.6);
+  backdrop-filter: calc(60%);
+`;
+
+export const Bio = styled.div`
+  font-weight: bold;
+  color: white;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  textarea {
+    border-radius: 8px;
+    border: 2px solid white;
+    background-color: rgba(255, 255, 255, 0.3);
+    padding: 15px 10%; 
+    resize: none;
+  }
+  label {
+    margin: 0 0 10px 0;
+  }
 `;
