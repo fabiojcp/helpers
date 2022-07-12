6db4d85158746ed74caaf57dce378240020ff152
Fábio Casanova <98760788+Fabiojcp@users.noreply.github.com>
2022-07-12 19:10:53 -0300

    feat: animation menu user unfinished

--- src/components/userMenu/styles.js
+++ src/components/userMenu/styles.js
@@ -1,6 +1,23 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const animation = keyframes`
+  0% {
+    transform: translateY(-100%);
+    transform: scale(0)
+
+  }
+  80% {
+    transform: translateY(1.1);
+    transform: scale(1.1)
+  }
+  100% {
+    transform: translateY(1);
+    transform: scale(1)
+  }
+`;
 
 export const ModalUser = styled.div`
+  animation: ${animation} 1s;
   position: absolute;
   display: flex;
   flex-direction: column;
@@ -11,13 +28,14 @@ export const ModalUser = styled.div`
   border-bottom-left-radius: 12px;
   border-bottom-right-radius: 12px;
   z-index: 1;
-  
+
   p {
     margin: 6px;
     color: #d9d9d9;
     font-size: 12px;
   }
   button {
+    animation: ${animation} 1s;
     background-color: #ffffff30;
     border: none;
     font-family: "Inter";
